152b1b914b606e7746855d65ab1bbca411c4d6c9
Michael Boerman <michaelboerman@hey.com>
2024-02-23 11:43:17 -0500

    tests added

---
+++ tests/test_package.py
@@ -0,0 +1,9 @@
+from example_package_michaelboerman.example import add_one
+
+def test_add_one():
+    assert add_one(1) == 2
+    assert add_one(0) == 1
+    assert add_one(-1) == 0
+    assert add_one(-2) == -1
+
+
