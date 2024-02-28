82b8c8b4bf2864433524b51e07b0b142ce636a1a
Michael Boerman <michaelboerman@hey.com>
2024-02-28 16:42:50 -0500

    tests added for input/output type validation

---
+++ tests/test_input_output_types.py
@@ -0,0 +1,18 @@
+import pytest
+from example_package_michaelboerman.example import add_one
+
+def test_expected_output_types():
+    
+    # expected inputs
+    assert isinstance(add_one(5), int)
+    assert isinstance(add_one(3.5), float)
+    
+def test_unexpected_input_types():
+    
+    # string input
+    with pytest.raises(TypeError):
+        add_one('abc')
+
+    # boolean input
+    with pytest.raises(TypeError):
+        add_one(True)
